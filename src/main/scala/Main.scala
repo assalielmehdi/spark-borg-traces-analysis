7eac0bb6e6a81214bbf98992c060b0b58028a97a
El Mehdi ASSALI <assalielmehdi@gmail.com>
2021-01-01 16:11:36 +0100

    :art: Refactor local data directories

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,23 +2,23 @@ import org.apache.spark.sql.SparkSession
 
 object Main {
   def main(args: Array[String]): Unit = {
-    if (args.length != 3) {
+    if (args.length != 5) {
       println("Invalid arguments")
       sys.exit(1)
     }
 
-    val jobLogs = args(1)
-    val machineLogs = args(2)
+    val machineEvents = args(1)
+    val jobEvents = args(2)
+    val taskEvents = args(3)
+    val taskUsage = args(4)
+
 
     val spark = SparkSession.builder
       .master("local")
       .appName("Data Management Project")
       .getOrCreate
 
-    val jobLogsDf = spark.read.option("header", "true").csv(jobLogs)
-    jobLogsDf.foreach(row => println(row))
-
-    val machineLogsDf = spark.read.option("header", "true").csv(machineLogs)
+    val machineLogsDf = spark.read.option("header", "true").csv(machineEvents)
     machineLogsDf.foreach(row => println(row))
   }
 
